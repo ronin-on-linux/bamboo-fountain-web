ab8b4538d52db87c6c149fff8fb6d85b67c34c60
ronin-on-linux <ronin.on.linux.3d@gmail.com>
2026-05-18 17:09:19 -0600

    Update sw.js
    correcting file path to manifest

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE = "bamboo-v1";
 const ASSETS = [
   "/bamboo-fountain-web/",
   "/bamboo-fountain-web/index.html",
-  "/bamboo-fountain/manifest.json",
+  "/bamboo-fountain-web/manifest.json",
   "https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400&family=IBM+Plex+Mono:wght@400;700&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
