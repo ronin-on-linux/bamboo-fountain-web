b574e6f50fababf512129f88b2e78804e1f012b8
ronin-on-linux <ronin.on.linux.3d@gmail.com>
2026-05-18 15:54:13 -0600

    Add files via upload
    manifest.json and service worker js scripts for web app PWA

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "bamboo-v1";
+const ASSETS = [
+  "/bamboo-fountain-web/",
+  "/bamboo-fountain-web/index.html",
+  "/bamboo-fountain/manifest.json",
+  "https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400&family=IBM+Plex+Mono:wght@400;700&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request))
+  );
+});
